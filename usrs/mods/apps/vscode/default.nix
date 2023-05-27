28a892e0531fa1d27dd6027d0c4eae1f5ddaf5e8
YorQat <qarkdev+gh@gmail.com>
2023-05-27 18:13:19 +0800

    customize vscode

--- usrs/mods/apps/vscode/default.nix
+++ usrs/mods/apps/vscode/default.nix
@@ -3,6 +3,20 @@
 
   programs.vscode = {
     enable = true;
+    userSettings = {
+      "window.titleBarStyle" = "custom";
+      "window.titleSeparator" = "  <->  ";
+
+      "workbench.colorTheme" = "Catppuccin Mocha";
+      # "workbench.colorTheme" = "Gruvbox Light Hard";
+      "workbench.iconTheme" = "material-icon-theme";
+      "workbench.editor.titleScrollbarSizing" = "large";
+
+      "explorer.compactFolders" = false;
+      "explorer.sortOrder" = "filesFirst";
+
+      "editor.fontFamily" = "'JetBrainsMono', 'monospace', monospace";
+    };
     extensions = with pkgs.vscode-extensions; [
       vscodevim.vim
       yzhang.markdown-all-in-one
