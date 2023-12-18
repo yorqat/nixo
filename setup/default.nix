9f53d150e2a310189ffc3fd5acd8fceb3840f9db
YorQat <qarkdev+gh@gmail.com>
2023-12-18 15:41:16 +0800

    proper default behavior

--- setup/default.nix
+++ setup/default.nix
@@ -50,8 +50,8 @@ in {
     # for enabling secure boot
     # https://nixos.wiki/wiki/Secure_Boot
     secureBoot = {
-        bootspec = true;
-        lanzaboote = true;
+        bootspec = false;
+        lanzaboote = false;
     };
 
     # This value determines the NixOS release from which the default
